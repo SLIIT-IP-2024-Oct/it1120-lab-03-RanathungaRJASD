bf28be895d6cd54913ed4f60eb2b42db068c6597
RanathungaRJASD <IT24103843@my.sliit.lk>
2024-11-09 08:00:05 +0530

    Add files via upload

---
+++ Q1A/IT24104072Lab3Q1A.java
@@ -0,0 +1,15 @@
+public class TextLab3014 (
+
+public static void main(String[] args)
+
+// Declare the variples double priceperitg, Quantity, totalamount;
+
+// Create a Scammer object to reed Input Scanner Input new Scanner(System.in);
+
+// Prompt the user to enter the price per kilogram of rice System.out.print("Enter the price of 1kg of rices"); pricepertg Input.nextDouble();
+
+// Prompt the user to enter the number of kilograms they want to buy System.out.print("Enter the number of kilograms you want to buy: ") quantity Input.nextDouble();
+
+// Calculate the total amount to be paid totalAmount pricepertg quantity:
+
+// Displey the total amount System.out.println(); System.out.println("The total amount is totalamuurt)
